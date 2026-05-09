4abe85bd5f92f88ac7ebf7c45320ce25bc288483
SalmanKhanKKKUK <salmankhan737400@gmail.com>
2026-05-08 22:23:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello World! about page");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Login Page Welcomed</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
